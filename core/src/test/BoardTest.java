e289cf98e18046a2384da08255be104baa820e53
filpez <joaofilipepereiracosta@hotmail.com>
2016-05-11 15:33:13 +0100

    Tests

---
+++ core/src/test/BoardTest.java
@@ -0,0 +1,29 @@
+package test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+
+public class BoardTest {
+
+	@Test
+	public void testPlayerMovement() {
+		//Test if the players move correectly.
+	}
+	
+	@Test
+	public void testBuyingAPropriety() {
+		//Test if the players buys a propriety correctly.
+	}
+	
+	@Test
+	public void testEffect() {
+		//Test various effects from cards or spaces, example: Go to Jail, Pay.
+	}
+	
+	@Test
+	public void testWinLose() {
+		//Test winning and losing conditions.
+	}
+}
---
+++ core/src/test/ProprietyTest.java
@@ -0,0 +1,50 @@
+package test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import monopoly.logic.BuildingLot;
+import monopoly.logic.Group;
+import monopoly.logic.Player;
+import monopoly.logic.Propriety;
+
+public class ProprietyTest {
+
+	
+	@Test
+	public void testProprietyBuilder() {
+		Group g1 = new Group("Red");
+		int[] p1_rents= {20,100,250,500,1000};
+		Propriety p1 = new BuildingLot("Porto", g1, 300, p1_rents);
+		
+		assertEquals(p1.getPrice(), 300);
+		assertEquals(p1.getName(), "Porto");
+		assertEquals(p1.isMortaged(), false);
+		assertEquals(p1.getGroup(), new Group("Red"));
+	}
+	
+	@Test
+	public void testBuildingLotRents() {
+		Group g1 = new Group("Red", 150);
+		int[] p1_rents= {20,100,250,500,1000};
+		Propriety p1 = new BuildingLot("Porto", g1, 300, p1_rents);
+		Propriety p2 = new BuildingLot("Lisboa", g1, 300, p1_rents);
+		
+		Player player = new Player("Derp");
+		player.buy(p1);
+		assertEquals(p1.getRent(0), 20);
+		assertFalse(p1.canAddHouse());
+		
+		player.buy(p2);
+		assertEquals(p1.getRent(0), 40);
+		//assertTrue(p1.canAddHouse());
+		
+		
+		((BuildingLot)p1).addHouse();
+		assertEquals(p1.getRent(0), 100);
+	}
+	
+	//Other Rents
+
+}
