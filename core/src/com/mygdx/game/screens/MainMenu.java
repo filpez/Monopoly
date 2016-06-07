4b77e7938c187fbf2c8c78596c372fb9e406a5c5
filpez <joaofilipepereiracosta@hotmail.com>
2016-06-07 14:49:47 +0100

    Sorry

--- core/src/com/mygdx/game/screens/MainMenu.java
+++ core/src/com/mygdx/game/screens/MainMenu.java
@@ -285,7 +285,7 @@ public class MainMenu  implements Screen {
         button.addListener(new ChangeListener() {
             public void changed (ChangeEvent event, Actor actor) {
                 if (button.getText().toString().equals("Start")){//) && game.controller.getBoard() != null
-                    if (game.controller.getBoard() != null)
+                    if (game.controller.getBoard() == null)
                         return;
                     dialog.hide();
                     game.setScreen(new BoardScreen(game));
